2f8a0fbcde1172f5a5d7c449c670a806b7a9615b
KillerQueen3 <sydsws123@gmail.com>
2020-10-12 20:39:56 +0800

    初次发布

--- MouseM@ster/Form1.Designer.cs
+++ MouseM@ster/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.setTimerBtn = new System.Windows.Forms.Button();
 			this.hourTB = new System.Windows.Forms.TextBox();
 			this.minTB = new System.Windows.Forms.TextBox();
@@ -333,6 +334,7 @@
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.menuStrip1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MainMenuStrip = this.menuStrip1;
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
